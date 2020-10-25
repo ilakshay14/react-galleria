a0bb8bb00c0e40aa56c7cfbf6e29aa1b2fe0f54c
ilakshay14 <ihybrid11@gmail.com>
2020-10-25 21:20:51 +0530

    initial commit. min features

--- README.md
+++ README.md
@@ -1,24 +1,31 @@
-<h1>React Boilerplate</h1>
+<h1>Galleria</h1>
+A React app where you can upload your shots for display.
 
-Welcome to my basic React boilerplate. Thanks for cloning it.
-
-<h4>Boilerplate Features</h4>
-1. Some basic UI elements are available
-    a. Anchor
-    c. Image
-    d. Input
-    e. Button
-
-2. Basic WEBPACK config
-3. Muli Google Font
-4. Styled Components
-5. Global styles using Styled components
-6. Theme provider using Styled components
-
-
-<h4>Instructions</h4>
+<h2>Features</h2>
 <ul>
-    <li>use command YARN/NPM</li>
-    <li>yarn start to run</li>
-    <li>yarn build to get the production build</li>
+    <li>set profile photo</li>
+    <li>set cover/feature photo</li>
+    <li>upload your shots</li>
+</ul>
+
+#Development in progress!
+
+<h2>Disclaimers</h2>
+<ul>
+    <li>default cover photo is taken from Unsplash</li>
+    <li>default profile photo is taken from Unsplash</li>
+</ul>
+
+<h2>Tech Stack</h2>
+<h4>Frontend</h4>
+<ul>
+    <li>ReactJS</li>
+    <li>Styled Components</li>
+</ul>
+
+<h4>Backend</h4>
+<ul>
+    <li>Node - Express</li>
+    <li>Multer</li>
+    <li>MongoDB</li>
 </ul>
--- src/App.js
+++ src/App.js
@@ -1,23 +1,243 @@
-import React, { Component } from 'react';
-import styled from 'styled-components';
+import React, { useEffect, useState } from "react";
+import styled from "styled-components";
+import axios from 'axios';
+import { FadeIn } from './theme/Animations';
+import { FlexColumn, FlexRow } from './theme/defaultStyles';
 
-const StyledLayout = styled.div`
-    padding: 5%;
-    color: ${ ({ theme }) => theme.colors.text};
+const Wrapper = styled.div`
+    width: 70%;
+    height: 100vh;
+    margin: 0 auto;
+    padding: 10px;
+    //background-color: #f0f2f5;
+    //border: 1px solid black !important;
 
-    & h1 {
-        font-size: 350%
+    input[type="file"]{
+        /* border: 1px solid black; */
+            display: none;
+        }
+
+    .cover-photo{
+        width: 100%;
+        height: 300px;
+        position: relative;
+
+        .upload-icon{
+            width: 40px;
+            height: 40px;
+            background-color: #f0f2f5;
+            border: 3px solid black;
+            border-radius: 50%;
+            position: absolute;
+            bottom: -20px;
+            right: 10px;
+            font-size: 150%;
+            text-align: center;
+            cursor: pointer;
+            ${FlexColumn};
+
+            transition: all 0.1s linear;
+
+            :hover{
+                box-shadow: 1px 4px 4px rgba(0, 0, 0, 0.25);
+                transform: scale(1.2);
+            }
+
+            label{
+                line-height: 100%;
+                cursor: pointer;
+            }
+        }
+
+        img{
+            width: 100%;
+            height: 100%;
+            object-fit: cover;
+            object-position: center;
+        }
+
+        .profile-photo{
+            width: 200px;
+            height: 200px;
+
+            position: absolute;
+            bottom: -50px;
+            left: 20px;
+            border-radius: 50%;
+            border: 3px solid black;
+
+            img{
+                width: 100%;
+                height: 100%;
+                border-radius: 50%;
+                
+                object-fit: cover;
+                //object-position: top;
+            }
+
+            label{
+                border: 1px solid black;
+                line-height: 100%;
+                cursor: pointer;
+                width: 30px;
+                height: 30px;
+                background-color: #f0f2f5;
+                border: 3px solid black;
+                border-radius: 50%;
+                position: absolute;
+                bottom: 5px;
+                left: 20px;
+                font-size: 130%;
+                text-align: center;
+                ${FlexColumn};
+            }
+
+        }
     }
+
+    .file-input-wrapper{
+
+        /* border: 1px solid black; */
+        width: 95%;
+        height: 40px;
+        margin-top: 70px;
+
+        ${FlexRow}
+        justify-content: flex-end;
+    
+        .file-input{
+            
+            display: inline-block;
+            width: 30%;
+            height: 40px;
+            border-radius: 20px;
+            background-color: white;
+            box-shadow: -4px 4px 4px rgba(0, 0, 0, 0.25);
+            padding: 3px;
+
+            font-size: 120%;
+            text-align: center;
+            line-height: 35px;
+            cursor: pointer;
+
+            
+        }
+    }
+
+    
+
+    .gallery{
+        margin: 20px auto;
+        //border: 1px solid black;
+        width: 100%;
+        height: auto;
+        ${FlexRow};
+        justify-content: space-evenly;
+        align-content: flex-start;
+        align-items: flex-start;
+
+        img{
+            margin-top: 20px;
+            flex: 0 0 47%;
+            width: 47%;
+            height: 250px;
+            border-radius: 5px;
+            object-fit: cover;
+            object-position: center;
+            animation: ${FadeIn} ease-in-out forwards 1s;
+
+        }
+    }
+    
 `;
 
-class App extends Component {
-    render() {
-        return (
-            <StyledLayout>
-                <h2>Hello World!</h2>
-            </StyledLayout>
-        );
+
+const App = () => {
+
+    const [photos, addPhoto] = useState([]);
+    const [cover, setCover] = useState("https://images.unsplash.com/photo-1441974231531-c6227db76b6e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1951&q=80");
+    const [ profile, setProfile ] = useState("https://images.unsplash.com/photo-1561055657-b9e0bf0fa360?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80");
+
+    useEffect(()=>{
+        axios.get('http://192.168.29.173:3000/images').then(response => {
+            console.log(response.data);
+            addPhoto(response.data)
+        })
+    },[]);
+
+    const uploadFile = (e) => {
+        //e.preventDefault();
+        let data = new FormData();
+        
+        let myImage = e.target.files[0];
+        console.log(myImage);
+        
+        
+        let blobURL = URL.createObjectURL(e.target.files[0]);
+
+        data.append("image", myImage);
+        
+        console.log(data);
+
+        if (e.target.name === "cover") {
+            setCover(blobURL);
+            return;
+        }
+
+        if (e.target.name === "profile") {
+            setProfile(blobURL);
+            return;
+        }
+
+        
+
+        axios.post('http://192.168.29.173:3000/upload',
+            data).then(response => {
+                if(response.status === 201){
+                    let myPhotos = photos;
+                    myPhotos.push(blobURL);
+                    addPhoto([...myPhotos]);
+                }
+            })
+            .catch(e => console.log(e));
     }
+
+    return (
+        <Wrapper>
+            <div className="cover-photo">
+                <div className="upload-icon">
+                
+                    <label className="">
+                        <input type="file" name="cover" onChange={uploadFile} />
+                        <ion-icon name="camera"></ion-icon>
+                    </label>
+
+                </div>
+                <div className="profile-photo">
+                    <img src={profile}/>
+                    <label className="">
+                        <input type="file" name="profile" onChange={uploadFile} />
+                        <ion-icon name="pencil"></ion-icon>
+                    </label>
+                </div>
+                <img src={cover} />
+            </div>
+            {/* <form encType="multipart/form-data"> */}
+            <div className="file-input-wrapper">
+                <label className="file-input">
+                    <input type="file" onChange={uploadFile} />
+                    Choose a file
+                </label>
+            </div>
+            {/* </form> */}
+            <div className="gallery">
+                {
+                    photos.map(photo => <img key={photo} src={photo} />)
+                }
+
+            </div>
+        </Wrapper>
+    );
 }
 
 export default App;
